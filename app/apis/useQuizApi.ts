243bc70c188e0deda0be7d6c254d478ecb69bfe7
Asher Lien <birdym.tw@gmail.com>
2025-09-13 11:31:23 +0800

    wip

--- app/apis/useQuizApi.ts
+++ app/apis/useQuizApi.ts
@@ -1,8 +1,36 @@
 import { useRequestApi } from '~/composables'
 import { PublicRequestUrl } from '~/enum'
 
-export const useQuizpi = {
+export const useQuizApi = {
   get: async () => {
+    return await useRequestApi(PublicRequestUrl.Dev, {
+      method: 'GET',
+      server: false,
+      lazy: true,
+    })
+  },
+  ge2t: async () => {
+    return await useRequestApi(PublicRequestUrl.Dev, {
+      method: 'GET',
+      server: false,
+      lazy: true,
+    })
+  },
+  ge23t: async () => {
+    return await useRequestApi(PublicRequestUrl.Dev, {
+      method: 'GET',
+      server: false,
+      lazy: true,
+    })
+  },
+  ge233t: async () => {
+    return await useRequestApi(PublicRequestUrl.Dev, {
+      method: 'GET',
+      server: false,
+      lazy: true,
+    })
+  },
+  ge2233t: async () => {
     return await useRequestApi(PublicRequestUrl.Dev, {
       method: 'GET',
       server: false,
